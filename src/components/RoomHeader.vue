fdbbf9ee6bffa9ff4c97dd0e01f2e1da625dfe85
Juliano Penna <julianopenna@gmail.com>
2019-03-10 11:24:40 +0800

    Add Room Header

---
+++ src/components/RoomHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="flex-split">
+      <!-- Left -->
+      <div>
+        <!-- Duration -->
+        <div>Duration</div>
+
+        <!-- Color -->
+        <div>Color</div>
+      </div>
+
+      <!-- Right -->
+      <div>
+        <el-button type="primary" plain>Patterns</el-button>
+        <el-button type="success" plain>Start</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RoomHeader',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RoomHeader />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import RoomHeader from '@/components/RoomHeader.vue';
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld,
+    RoomHeader,
   },
 };
 </script>
