2f7890070976ef7adf4f076cf381eb83ccc05adc
Juliano Penna <julianopenna@gmail.com>
2019-03-10 11:23:43 +0800

    Custom initial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,45 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
+    'vue/multiline-html-element-content-newline': 0,
+    'import/prefer-default-export': 0,
+    'vue/max-attributes-per-line': [2, {
+      singleline: 4,
+      multiline: {
+        max: 1,
+        allowFirstLine: false
+      }
+    }],
+    // This rule enforces a line break before tag's closing brackets.
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'always',
+    }],
+    'vue/singleline-html-element-content-newline': 0,
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+      ]
+    }],
+    'consistent-return': 0,
+    'arrow-body-style': 'warn',
+    'no-restricted-syntax': 0,
   },
 };
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  configureWebpack: {
+    devServer: {
+      port: process.env.PORT || 3400,
+    },
+  },
+};
