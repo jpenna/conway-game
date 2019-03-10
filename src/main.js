2f8cea0342e273736e1b415ce02095816e62fb5b
Juliano Penna <julianopenna@gmail.com>
2019-03-10 11:24:22 +0800

    Custom styles

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import ElementUI from 'element-ui';
+import App from '@/App.vue';
+import router from '@/router';
+import locale from 'element-ui/lib/locale/lang/en';
+import store from '@/store';
+import '@/registerServiceWorker';
+
+import '@/styles/index.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI, { locale });
+
 
 new Vue({
   router,
