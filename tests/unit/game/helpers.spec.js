308f36340d1fdb5bf2c2bd71eb64bd969cdb6133
Juliano Penna <julianopenna@gmail.com>
2019-03-16 21:57:44 +0800

    Add one test for helper + description third person

--- tests/unit/game/helpers.spec.js
+++ tests/unit/game/helpers.spec.js
@@ -11,30 +11,30 @@ describe('Helpers', () => {
   });
 
   describe('getMousePosition', () => {
-    it('Calculate the mouse position correctly', () => {
+    it('Calculates the mouse position correctly', () => {
       const res = helpers.getMousePosition(event, maxHeight, maxWidth);
       expect(res).to.deep.equal([event.offsetX, event.offsetY]);
     });
 
-    it('Set to 0 if X is negative', () => {
+    it('Sets to 0 if X is negative', () => {
       event.offsetX = -1;
       const res = helpers.getMousePosition(event, maxHeight, maxWidth);
       expect(res).to.deep.equal([0, event.offsetY]);
     });
 
-    it('Set to (max value - 1) if (beyond OR equal) X borders', () => {
+    it('Sets to (max value - 1) if (beyond OR equal) X borders', () => {
       event.offsetX = 500;
       const res = helpers.getMousePosition(event, maxHeight, maxWidth);
       expect(res).to.deep.equal([maxWidth - 1, event.offsetY]);
     });
 
-    it('Set to 0 if Y is negative', () => {
+    it('Sets to 0 if Y is negative', () => {
       event.offsetY = -1;
       const res = helpers.getMousePosition(event, maxHeight, maxWidth);
       expect(res).to.deep.equal([event.offsetX, 0]);
     });
 
-    it('Set to (max value - 1) if (beyond OR equal) Y borders', () => {
+    it('Sets to (max value - 1) if (beyond OR equal) Y borders', () => {
       event.offsetY = 500;
       const res = helpers.getMousePosition(event, maxHeight, maxWidth);
       expect(res).to.deep.equal([event.offsetX, maxHeight - 1]);
@@ -42,18 +42,19 @@ describe('Helpers', () => {
   });
 
   describe('Run Round', () => {
-    it('Check each live cell for survival');
+    it('Checks each live cell for survival');
     it('Don\'t revive cells that are out of the canvas bound');
-    it('Revive dead cells flagged by survivors with 3 neighbors');
-    it('Revive dead cells with mixed colors of the 3 saviors');
-    it('Remove the cells that didn\'t survive the round');
+    it('Revives dead cells flagged by survivors with 3 neighbors');
+    it('Revives dead cells with mixed colors of the 3 saviors');
+    it('Removes the cells that didn\'t survive the round');
   });
 
   describe('Check survivors', () => {
-    it('Skip counting the current cell as neighbor');
+    it('Skips counting the current cell as neighbor');
+    it('Skips neighboring cells that are out of bounds');
     it('Counts the live cells correctly');
-    it('Flag the neighboring dead cells correctly');
-    it('For a neighboring dead cell, flag with right position, savior\'s color and amount');
-    it('For a neighboring dead cell, if has more than 1 neighbor, update information correctly');
+    it('Flags the neighboring dead cells correctly');
+    it('For a neighboring dead cell, flags with right position, savior\'s color and amount');
+    it('For a neighboring dead cell, if has more than 1 neighbor, updates information correctly');
   });
 });
