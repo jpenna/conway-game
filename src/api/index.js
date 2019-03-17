dddf2d2bc93cdd86bf1ee42d69d69548076a32e9
Juliano Penna <julianopenna@gmail.com>
2019-03-18 03:07:06 +0800

    Quick fix for tests: Websocket not found

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,12 @@
+// ! Until I find how to initialize Websocket before starting tests with mocha
+if (process.env.NODE_ENV === 'test') {
+  global.WebSocket = class Websocket {
+    send() {} // eslint-disable-line class-methods-use-this
+
+    addEventListener() {} // eslint-disable-line class-methods-use-this
+  };
+}
+
 const socket = new WebSocket(`ws://${process.env.VUE_APP_WS_URL}`);
 
 // Always send JSON
